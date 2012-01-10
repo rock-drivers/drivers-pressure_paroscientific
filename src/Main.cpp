a518bcb27383b33c3bb0b38c3f75da7bf8ea562b
hbuettner <hbuettner@hbuettner-Precision-M4500.(none)>
2012-01-10 16:06:55 +0100

    forgot to commit

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include <pressure-paroscientific/Dummy.hpp>
+#include <pressure_paroscientific/Driver.hpp>
 
 int main(int argc, char** argv)
 {
-	dummy_project::DummyClass dummyClass;
-	dummyClass.welcome();
+	pressure_paroscientific::Driver driver;
+    driver.open("udpserver://4446");
+
+    while (true) {
+        driver.collectPeriodicData();
+        pressure_paroscientific::ParoData data = driver.getData();
+        std::cout << data.time.toString() << "    ";
+        driver.dumpData();
+    }
 
 	return 0;
 }
