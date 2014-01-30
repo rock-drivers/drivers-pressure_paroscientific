3dff3a6472e461b81fb19cbc7d07ff04166385b1
Vincent <vincent@vincent-Marum.(none)>
2014-01-30 12:31:20 +0100

    modification of the extractPacket function in order to cope with the 0001 header

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -21,65 +21,117 @@ using namespace boost;
 
 
 Driver::Driver()
-    : iodrivers_base::Driver(64)
+: iodrivers_base::Driver(64)
 {
-    data.depth = 0;
+	data.depth = 0;
 }
 
 void Driver::open(const std::string & uri)
 {
-    openURI(uri);
+	openURI(uri);
 }
 
 
 void Driver::collectPeriodicData()
 {
-    string message;
-    try {
-        message = read(2000);
-    } catch (iodrivers_base::TimeoutError&) {
-        cout << "pressure_paroscientific timeout" << endl;
-        return;
-    }
+	string message;
+	try {
+		message = read(2000);
+	} catch (iodrivers_base::TimeoutError&) {
+		cout << "pressure_paroscientific timeout" << endl;
+		return;
+	}
 
-    message.erase(0,1);
-    //cout << "Data received: " << message;
-    data.time = base::Time::now();
-    data.depth = atof(message.c_str());
+	message.erase(0,5); // remove the star + "0001" so 5 characters
+	//cout << "Data received: " << message;
+	data.time = base::Time::now();
+	data.depth = atof(message.c_str());
 }
 
 pressure_paroscientific::ParoData Driver::getData() const
 {
-    return data;
+	return data;
 }
 
 void Driver::dumpData() const
 {
-    cout << setprecision(3) << data.time << " "
-         << setprecision(3) << data.depth <<  endl;
+	cout << setprecision(3) << "TIME = " << data.time << setprecision(3) << "  DEPTH = "<< data.depth<<endl;
 }
 
 std::string Driver::read(int timeout)
 {
-//  *ppppppp.ppp
-
-    char buffer[MAX_PACKET_SIZE];
-    size_t packet_size = readPacket(reinterpret_cast<uint8_t *>( buffer), MAX_PACKET_SIZE, 5000, timeout);
-    return string(buffer, packet_size);
+	// Format received = *ppppppp.ppp
+	char buffer[MAX_PACKET_SIZE];
+	size_t packet_size = readPacket(reinterpret_cast<uint8_t *>( buffer), MAX_PACKET_SIZE, 5000, timeout);
+	return string(buffer, packet_size);
 }
 
+/** DecToHex
+    Print an Hex value from a decimal value
+
+    @param  n Type int
+
+    @return string
+*/
+string DecToHex(int n)
+{
+    ostringstream oss;
+    oss<<hex<<n;
+    return oss.str();
+}
+
+/** extractPacket
+    Extract the packet in the buffer
+
+    @param buffer      Type uint8_t const
+    @param buffer_size Type size_t
+
+    @return int -buffer size or buffer size of the packet
+*/
 int Driver::extractPacket(const uint8_t *buffer, size_t buffer_size) const
 {
+//    for (int i = 0 ; i < (int)buffer_size ; i++ ){
+//        std::cout<<"("<<i<<")"<<"="<<DecToHex((int)buffer[i])<<" ";
+//        if(i%20==0 &&i!=0)
+//            std::cout<<""<<std::endl;
+//    }
+//    std::cout<<""<<std::endl;
 
-    for (size_t i = 0; i < buffer_size; i++) {
-        if (buffer[i] == '\n') {
-            if ( i < 12 ) {
-                return -(i + 1);
-            }
-            return i + 1;
-        }
-    }
-    return 0;
+	if(buffer_size > 0){
+		if(buffer[0] == 0x2A){ // 0x2A = * in ASCII code
+			//std::cout<<"[extractPacket]: We have a valid header"<<std::endl;
+
+			if(buffer[buffer_size-1] == '\n'){
+				//std::cout<<"[extractPacket]: We have a full packet"<<std::endl;
+				//there is a full packet in buffer, starting at the first buffer byte. Return the packet size.
+				//That data will be copied back to the buffer given to readPacket.
+				return buffer_size;
+			}
+			else {
+				//a packet begins at the first byte of buffer, but the end of the packet is not in buffer yet. Return 0.
+				//std::cout<<"[extractPacket]: First byte but not the end of the packet YET"<<std::endl;
+				return 0;
+			}
+		}//byte1 is not "*"
+		else{
+			//there is the beginning of a packet but it is not starting at the first byte of buffer.
+			//In that case, return -position_packet_start, where position_packet_start is the position of the packet in buffer.
+			for (unsigned int pos_pack_start = 0 ; pos_pack_start < buffer_size - 1 ; pos_pack_start++){
+				if(buffer[pos_pack_start] == 0x2A){ // 0x2A = * in ASCII code
+					//std::cout<<"[extractPacket]: We have a header but not at pos 1"<<std::endl;
+					return -pos_pack_start;
+				}
+			}
+		}
+
+	}//buffer_size <= 0
+	else{
+		//there is no packet in the buffer. In that case, return -buffer_size to
+		//discard all the data that has been gathered until now.
+		std::cout<<"[extractPacket]: no packet in buffer"<<std::endl;
+		return -buffer_size;
+	}
+
 }
 
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <pressure_paroscientific/Driver.hpp>
 
+
+void usage()
+{
+	std::cerr << "You must enter for example : serial:///dev/ttyACM3:115200 " << std::endl;
+}
+
+
 int main(int argc, char** argv)
 {
-	pressure_paroscientific::Driver driver;
-    driver.open("udpserver://4446");
 
-    while (true) {
-        driver.collectPeriodicData();
-        pressure_paroscientific::ParoData data = driver.getData();
-        std::cout << data.time.toString() << "    ";
-        driver.dumpData();
-    }
+	if (argc != 2)
+	{
+		usage();
+		return 1;
+	}
+
+	pressure_paroscientific::Driver driver;
+	driver.open(argv[1]);
+
+	while (true) {
+		driver.collectPeriodicData();
+		pressure_paroscientific::ParoData data = driver.getData();
+		std::cout << data.time.toString() << "   ";
+		driver.dumpData();
+	}
 
 	return 0;
 }
